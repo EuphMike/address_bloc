85369383208bf5e5118ebb465a1e1a2728669ad1
Mike Waddell <waddellmusic@gmail.com>
2017-06-01 16:16:38 +0000

    updateAssignment

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -82,28 +82,28 @@ require_relative '../models/address_book'
      describe "#import_from_csv2" do
        it "imports the correct number of entries" do
 
-         book.import_from_csv2("entries_2.csv")
+         book.import_from_csv("entries_2.csv")
          book_size = book.entries.size
 
          expect(book_size).to eql 3
        end
 
        it "imports the 1st entry" do
-        book.import_from_csv2("entries_2.csv")
+        book.import_from_csv("entries_2.csv")
 
         entry_one = book.entries[0]
         check_entry(entry_one, "Clementine", "614-444-4444", "Clem@puppy.bark")
        end
 
        it "imports the 2nd entry" do
-         book.import_from_csv2("entries_2.csv")
+         book.import_from_csv("entries_2.csv")
 
          entry_two = book.entries[1]
          check_entry(entry_two, "Kelsey", "614-400-5555", "Kelsey@waddell.com")
        end 
 
        it "imports the 3rd entry" do
-       book.import_from_csv2("entries_2.csv")
+       book.import_from_csv("entries_2.csv")
 
        entry_three = book.entries[2]
        check_entry(entry_three, "Mike", "614-555-5555", "Mike@euphmike.com")
